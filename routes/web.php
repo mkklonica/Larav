19579ec6d488d244024d79ba33bc96502c34780e
Mateusz Kłonica <mateuszklonica@gmail.com>
2021-11-26 18:28:18 +0100

    web.php and how is it working, routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('hello')->group(function () {
+    Route::get('/world', function () {
+        echo "Witaj w świecie";
+    });
+    Route::get('/{name?}/{age?}', function (String $name = "Nieznajomy", int $age = null) {
+    // ? po nazwach parametrów definiuje, że parametr jest opcjonalny 
+        echo "Siema ".$name;
+            if (is_null($age))
+                echo ". Nie podałeś wieku";
+            else 
+                echo ", masz ".$age." lat";
+    });
+});
+Route::redirect('/witaj', '/hello/world'); // przekierwoanie rzadania adresu 
+    // })->name('yoString');
+
+                        //przypisanie danej trasie jednoznacznej nazwy, np do przekierowań
+                        //redirect()->route('yoString')
+/* 
+    obsługa trasy
+    Route::get('/hello-word', "HelloWordController@hello");
+    odwołuje się do metody hello kontrolera-klasy HelloWordController 
+    ale narazie to tylko w ramach dygresji
+    Najpewniej przprzez routing okreslamy trasy jakimi mają podążać użytkownicy, kierując ich bezpośrednio
+    do wyznaczonych linków w zależności czy użytkownik jest zalogowany czy nie>
+    >> na ten moment to moje przemyślenia 
+
+    W dokumentacji znajdują się wszystkie standardowe metody obsługiwane przez Laravel
+    jak również niestandardowe tj. ::match , ::any  
+
+    Czym są rządania sparametryzowane ? 
+    Wychodzi na to, że to coś  niezbędnego w routing. Parametry URL
+*/
+?>
